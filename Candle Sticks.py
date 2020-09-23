ece1053510962323ae7707582b537e57cbea473d
Ben Williams <ben.williams21@outlook.com>
2020-09-23 09:54:36 +0100

    Much faster and accounts for missing minute data, removed strftime

--- Candle Sticks.py
+++ Candle Sticks.py
@@ -3,7 +3,6 @@ import plotly.graph_objects as go
 
 data = pd.read_csv(r"./GBPCHF_Ticks_10.08.2020-10.08.2020.csv")
 
-
 data.columns = ["Date", "open", "close", "Ask_vol", "Bid_vol"]
 
 data["Date"] = pd.to_datetime(data["Date"])
@@ -26,14 +25,14 @@ listofOpens = []
 listofCloses = []
 timeMH = []
 
-for i in range(0, 2000):
+for i in range(1, len(data["close"])):
 
     if i == 0 or Minutes[i] == Minutes[i - 1]:
         listOfPrices.append(round(closePrices[i], 5))
         maxPrice = max(listOfPrices)
         minPrice = min(listOfPrices)
 
-    elif Minutes[i] != Minutes[i - 1]:
+    elif Minutes[i] != Minutes[i - 1] and len(listOfPrices) != 0:
 
         openPrice = listOfPrices[-len(listOfPrices)]
         closePrice = listOfPrices[len(listOfPrices) - 1]
@@ -42,10 +41,13 @@ for i in range(0, 2000):
         listofMins.append(minPrice)
         listofOpens.append(openPrice)
         listofCloses.append(closePrice)
-        timeMH.append(data["Date"].dt.strftime("%H:%M")[i])
+        timeMH.append(data["Date"][i])
         listOfPrices.clear()
         counter += 1
 
+    else:
+        counter += 1
+        listOfPrices.clear()
 
 baseFigure = go.Figure(data=[go.Candlestick(x=timeMH,
                                             open=listofOpens,
